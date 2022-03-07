0d339e90376d905267b00bb79965b9cd9b03785c
Scott Dennis <scottlogandennis@gmail.com>
2022-03-07 15:19:19 +0200

    lesson 6

---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>Scotts Blog</h1>
+            <div className="links">
+                <a href="/">Home</a><br></br>
+                <a href="/">New Blog</a><br></br>
+                <a href="/">Home</a><br></br>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
